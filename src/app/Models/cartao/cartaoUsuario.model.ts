2e126e5229ac5ec0b9ebb08ba1ac69db7cdc163c
Victor Amaro <amaro.victor@hotmail.com>
2020-10-02 22:14:27 -0300

    up

---
+++ src/app/Models/cartao/cartaoUsuario.model.ts
@@ -0,0 +1,11 @@
+export class CartaoUsuarioModel {
+  carCodi: number;
+  bcCodi: number;
+  ifCodi: number;
+  usuCodi: number;
+  carDesc: string;
+  carLimit: number;
+  carDiaVenc: number;
+  carSald: number;
+  carFlAt: boolean;
+}
